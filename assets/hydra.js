2845d07fea7bd88a7f8550cfdc998b3e0b0ed726
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-05-14 11:39:40 +0200

    Add a makeshift websocket client and start fetching UTxO

--- assets/hydra.js
+++ assets/hydra.js
@@ -1,10 +1,34 @@
+console.log("connecting to hydra head at ws://127.0.0.1:4001")
+
+// Makeshift hydra client
+
+const protocol = window.location.protocol == "https:" ? "wss:" : "ws:";
+const conn = new WebSocket(protocol + "//127.0.0.1:4001?history=no");
+
+conn.addEventListener("message", e => {
+  const msg = JSON.parse(e.data);
+  switch (msg.tag) {
+    default:
+      console.log("Hydra websocket", "Received", msg);
+  }
+});
+
+async function getUTxO() {
+  const res = await fetch("http://127.0.0.1:4001/snapshot/utxo");
+  return res.json();
+}
+
+
+// Callbacks from forked doom-wasm
+
 let latestCmd = { forwardMove: 0 };
 
-function hydraSend(cmd) {
+async function hydraSend(cmd) {
   console.log("encode and submit transaction for", cmd);
 
-  console.log("short-cutting for now...")
-  latestCmd = cmd;
+  const utxo = await getUTxO();
+  console.log("spendable utxo", utxo);
+  return utxo;
 }
 
 function hydraRecv() {
